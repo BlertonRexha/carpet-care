c85bda4984c5fc8cd6cc8c663f077bec5a2b19e0
Blerton Rexha <blerton.rexha@real-digital.de>
2025-05-10 15:38:49 +0200

    is auth method

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,11 +15,28 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
   },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/LoginView.vue'),
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL || '/'),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = localStorage.getItem('isCarpetCareAuthenticated');
+
+  if (to.name !== 'login' && !isAuthenticated) {
+    next({ name: 'login' });
+  } else if (to.name === 'login' && isAuthenticated) {
+    next({ name: 'list' });
+  } else {
+    next();
+  }
 });
 
 export default router;
